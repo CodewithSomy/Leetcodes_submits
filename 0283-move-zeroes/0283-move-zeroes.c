32567da506cfbc554369d66c709387cd6522f684
CodewithSomy <somyisbezt@gmail.com>
2026-08-06 12:53:12 +0530

    Time: 0 ms (100%), Space: 20.4 MB (49.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,15 @@
+void moveZeroes(int *nums, int numsSize)
+{
+    int insert_pos = 0;
+    for (int i = 0; i < numsSize; ++i)
+    {
+        if (nums[i] != 0)
+        {
+            nums[insert_pos++] = nums[i];
+        }
+    }
+    for (int i = insert_pos; i < numsSize; ++i)
+    {
+        nums[i] = 0;
+    }
+}
