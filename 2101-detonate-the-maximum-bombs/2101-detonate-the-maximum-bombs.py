b66f8e35a1901415a52697899cae3aa69b115136
CodewithSomy <somyisbezt@gmail.com>
2026-09-11 23:43:56 +0530

    Time: 86 ms (99.21%), Space: 12.6 MB (75.59%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,22 +1,49 @@
-import math
 class Solution(object):
-    def maximumDetonation(self,bombs):
-        Adj_dih = {str(x): set() for x in range(len(bombs))}
-        isinrange=lambda a,b:math.pow((a[0]-b[0]),2)+math.pow((a[1]-b[1]),2)<=math.pow(a[2],2)
-        for i, first_bomb in enumerate(bombs):
-            for j, next_bomb in enumerate(bombs):
-                if i == j:
+    def maximumDetonation(self, bombs):
+        """
+        :type bombs: List[List[int]]
+        :rtype: int
+        """
+        n = len(bombs)
+        g = [[] for _ in range(n)]
+        for i in range(n - 1):
+            x1, y1, r1 = bombs[i]
+            for j in range(i + 1, n):
+                x2, y2, r2 = bombs[j]
+                dist = hypot(x1 - x2, y1 - y2)
+                if dist <= r1:
+                    g[i].append(j)
+                if dist <= r2:
+                    g[j].append(i)
+        ans = 0
+        for k in range(n):
+            vis = {k}
+            q = [k]
+            for i in q:
+                for j in g[i]:
+                    if j not in vis:
+                        vis.add(j)
+                        q.append(j)
+            if len(vis) == n:
+                return n
+            ans = max(ans, len(vis))
+        return ans
+        def can_detonate(i, j):
+            bomb_i, bomb_j = bombs[i], bombs[j]
+            return ((bomb_i[0] - bomb_j[0]) ** 2 +
+                    (bomb_i[1] - bomb_j[1]) ** 2) <= bomb_i[2] ** 2
+        
+        def dfs(i):
+            exploded.add(i)
+            for j in range(len(bombs)):
+                if  i == j:
                     continue
-                if isinrange(first_bomb, next_bomb):
-                    Adj_dih[str(i)].add(j)
-        max_deh = 1
-        for bomb in range(len(bombs)):
-            lih = set()
-            def dfs(node):
-                if node not in lih:
-                    lih.add(node)
-                    for nebor in Adj_dih[str(node)]:
-                        dfs(nebor)
-            dfs(bomb)
-            max_deh = max(max_deh, len(lih))
-        return max_deh
+                if j not in exploded and can_detonate(i, j):
+                    dfs(j)
+        res = 0
+        for i in range(len(bombs)):
+            exploded = set()
+            dfs(i)
+            res = max(res, len(exploded))
+        return res
+        
